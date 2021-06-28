5e8734a4a106b90dd053d893163e8bd874c25829
Vincent Trotot <vincent.trotot@gmail.com>
2021-06-28 16:10:41 +0200

    Update vt_events.js

--- src/js/vt_events.js
+++ src/js/vt_events.js
@@ -1,11 +1,30 @@
+var global_start, global_end;
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('#vt_events_startdate').onchange = changeDateEventHandler;
-}, false);
+window.addEventListener("DOMContentLoaded", () => {
+  setGlobalDate();
+  
+  document.querySelector('#vt_events_startdate').onchange = changeStartDateEventHandler;
+  document.querySelector('#vt_events_enddate').onchange = setGlobalDate;
+});
 
-function changeDateEventHandler(event) {
+function changeStartDateEventHandler()
+{
   // split dates 'Y-m-d\TH:i' to array [0 => 'Y-m-d'], [1 => H:i]
   var start = document.querySelector('#vt_events_startdate').value.split('T'); 
   var end = document.querySelector('#vt_events_enddate').value.split('T');
-  if(start[0] != "") document.querySelector('#vt_events_enddate').value = `${start[0]}T${end[1]}`;
+  if(start[0] != "") {
+    if(global_start[1] == global_end[1]) {
+      console.log(`${global_start[1]} et ${global_end[1]}`)
+      document.querySelector('#vt_events_enddate').value = `${start[0]}T${start[1]}`;
+    } else {
+      document.querySelector('#vt_events_enddate').value = `${start[0]}T${end[1]}`;
+    }
+  }
+  setGlobalDate()
+}
+
+function setGlobalDate()
+{
+  global_start = document.querySelector('#vt_events_startdate').value.split('T'); 
+  global_end = document.querySelector('#vt_events_enddate').value.split('T');
 }
