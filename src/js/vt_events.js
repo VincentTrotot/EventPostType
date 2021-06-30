1701a6f33642c1b12898cee5b4228c1bcb36fd78
Vincent Trotot <vincent.trotot@gmail.com>
2021-06-30 08:08:19 +0200

    FIX: update enddate only if event is on one day

--- src/js/vt_events.js
+++ src/js/vt_events.js
@@ -12,9 +12,8 @@ function changeStartDateEventHandler()
   // split dates 'Y-m-d\TH:i' to array [0 => 'Y-m-d'], [1 => H:i]
   var start = document.querySelector('#vt_events_startdate').value.split('T'); 
   var end = document.querySelector('#vt_events_enddate').value.split('T');
-  if(start[0] != "") {
+  if(start[0] != "" && global_start[0] == global_end[0]) {
     if(global_start[1] == global_end[1]) {
-      console.log(`${global_start[1]} et ${global_end[1]}`)
       document.querySelector('#vt_events_enddate').value = `${start[0]}T${start[1]}`;
     } else {
       document.querySelector('#vt_events_enddate').value = `${start[0]}T${end[1]}`;
